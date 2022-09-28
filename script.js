a15ff903c346a2a64a5c7ddd5ab48082bfb350ae
Cristian <christianchapa@yahoo.com>
2022-09-28 13:02:09 -0400

    Added players score

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0
+let comScore = 0
+
 const computerPlay = () => {
     const arrOfChoices = ['Rock', 'Paper', 'Scissors']
     const randomNum = Math.floor(Math.random() * 3)
@@ -14,27 +17,42 @@ const playRound = (playerSelection, computerSelection) => {
     } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
         return 'You both chose Scissors!'
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        comScore++
         return 'You lost! Your rock was eaten by paper'
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        playerScore++
         return 'You won! Your rock crushed scissors'
     } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        comScore++
         return 'You lost! Your paper was shred to pieces by scissors'
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        playerScore++
         return 'You won! Your paper ate rock'
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        comScore++
         return 'You lost! Your scissors was crushed by rock'
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        playerScore++
         return 'You won! Your scissors shred paper'
     }
 }
 
 const playerSelection = 'Rock';
-const computerSelection = computerPlay()
 
 const game = () => {
     for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerSelection));
+        const computerSelection = computerPlay()
+        (playRound(playerSelection, computerSelection));
+    }
+
+    if (playerScore > comScore) {
+        return 'Congragulations, You\'ve won!'
+    } else if (playerScore < comScore) {
+        'You\'ve lost, Bummer'
+    } else {
+        return 'Wow, you guys ended in a tie.'
     }
 }
 
-game()
+
+console.log(game);
