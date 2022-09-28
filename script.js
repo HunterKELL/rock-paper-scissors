271e449883ecc80c15a04222c6870af8af0e170c
Cristian <christianchapa@yahoo.com>
2022-09-28 11:58:26 -0400

    Wrote initial function for computerPlay

---
+++ script.js
@@ -0,0 +1,7 @@
+const computerPlay = () => {
+    const arrOfChoices = ['Rock', 'Paper', 'Scissors']
+    const randomNum = Math.floor(Math.random() * 3)
+    console.log(arrOfChoices[randomNum])
+} 
+
+computerPlay();
