fe943dd5e6760b26159bf0a6fdeeaff90bf65517
Cristian <christianchapa@yahoo.com>
2022-09-28 21:01:34 -0400

    Changed to rock paper scisscors to grass fire water

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let playerScore = 0
 let comScore = 0
 
 const computerPlay = () => {
-    const arrOfChoices = ['Rock', 'Paper', 'Scissors']
+    const arrOfChoices = ['Fire', 'Grass', 'Water']
     const randomNum = Math.floor(Math.random() * 3)
     console.log(arrOfChoices[randomNum])
     const comChoice = arrOfChoices[randomNum]
@@ -10,39 +10,39 @@ const computerPlay = () => {
 } 
 
 const playRound = (playerSelection, computerSelection) => {
-    if (playerSelection === 'Rock' && computerSelection === 'Rock') {
-        return 'You both chose Rock!'
-    } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
-        return 'You both chose Paper!'
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
-        return 'You both chose Scissors!'
-    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+    if (playerSelection === 'Fire' && computerSelection === 'Fire') {
+        return 'You both chose Fire!'
+    } else if (playerSelection === 'Grass' && computerSelection === 'Grass') {
+        return 'You both chose Grass!'
+    } else if (playerSelection === 'Water' && computerSelection === 'Water') {
+        return 'You both chose Water!'
+    } else if (playerSelection === 'Fire' && computerSelection === 'Water') {
         comScore++
-        return 'You lost! Your rock was eaten by paper'
-    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You lost! Fire was extinguished by Water'
+    } else if (playerSelection === 'Fire' && computerSelection === 'Grass') {
         playerScore++
-        return 'You won! Your rock crushed scissors'
-    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return 'You won! Fire burned Grass'
+    } else if (playerSelection === 'Grass' && computerSelection === 'Fire') {
         comScore++
-        return 'You lost! Your paper was shred to pieces by scissors'
-    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return 'You lost! Grass was burned by Fire'
+    } else if (playerSelection === 'Grass' && computerSelection === 'Water') {
         playerScore++
-        return 'You won! Your paper ate rock'
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'You won! Grass absorbed all the Water'
+    } else if (playerSelection === 'Water' && computerSelection === 'Grass') {
         comScore++
-        return 'You lost! Your scissors was crushed by rock'
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return 'You lost! Water was absorbed by Grass'
+    } else if (playerSelection === 'Water' && computerSelection === 'Fire') {
         playerScore++
-        return 'You won! Your scissors shred paper'
+        return 'You won! Water extinguished the Fire'
     }
 }
 
 const game = () => {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('Choose your Hand', 'Rock Paper Scissors').toLowerCase()
-        const computerSelection = computerPlay()
-        playRound(playerSelection, computerSelection);
-    }
+    //for (let i = 0; i < 5; i++) {
+    //    const playerSelection = prompt('Choose your Hand', 'Rock Paper Scissors').toLowerCase()
+    //    const computerSelection = computerPlay()
+    //    playRound(playerSelection, computerSelection);
+    //}
 
     if (playerScore > comScore) {
         return 'Congragulations, Youve won!'
